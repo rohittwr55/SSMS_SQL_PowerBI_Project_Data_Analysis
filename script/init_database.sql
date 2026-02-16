504f028c5723f55de536c904dfe1e3a5e37f7bde
rohittwr55 <32168434+rohittwr55@users.noreply.github.com>
2026-02-16 17:57:59 +0530

    Create init_database.sql

---
+++ script/init_database.sql
@@ -0,0 +1,15 @@
+
+use master;
+
+--Create Database Datawarehouse
+create Database DataWarehouse;
+
+use DataWarehouse;
+
+--Creating Schemas
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go
